6a5dae4b3cf2887f30e4ce3c0a82dce4e6fd61d6
CawaAlreadyTaken <daniele.cabassi01@gmail.com>
2022-10-25 15:02:48 +0200

    scheletro Prima Lezione

---
+++ PrimaLezione/index.js
@@ -0,0 +1,36 @@
+//Hello World! (output da console)
+
+
+//Variabili e tipi
+
+
+
+
+//Oggetti e dizionari
+
+
+
+
+
+//If
+
+
+
+
+
+//Operazioni
+
+
+
+
+//Cicli
+
+
+
+
+
+//Funzioni
+
+
+//Moduli
+
