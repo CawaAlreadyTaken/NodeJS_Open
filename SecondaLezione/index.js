4f88ea157fb079e939637438d9c536b5dcaf951f
CawaAlreadyTaken <daniele.cabassi01@gmail.com>
2022-11-08 12:41:42 +0100

    seconda lezione

---
+++ SecondaLezione/index.js
@@ -0,0 +1,23 @@
+// Nuova cartella
+// Cosa sono i Moduli di NodeJS?    https://www.npmjs.com/
+//
+// Modulo per gestire un bot telegram   https://www.npmjs.com/package/node-telegram-bot-api
+
+
+// Creazione di un bot con BotFather
+
+
+// Gestione dell'evento "message"
+
+
+
+
+
+
+
+
+
+
+
+
+// Moduli built-in, come "fs"
